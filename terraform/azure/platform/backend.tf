7b893f92eb9fa3ec14f771b2e91d452a99987b04
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-02-14 14:32:41 +0100

    backends terraform

--- terraform/azure/platform/backend.tf
+++ terraform/azure/platform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-      resource_group_name  = "demo-puresight-starter"
-      storage_account_name = "tfstateyfyg6"
-      container_name       = "streamx-commerce-accelerator-tfstate"
-      key                  = "platform.tfstate"
+    resource_group_name  = "demo-puresight-starter"
+    storage_account_name = "tfstateyfyg6"
+    container_name       = "streamx-commerce-accelerator-tfstate"
+    key                  = "platform.tfstate"
   }
 }
