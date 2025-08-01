7f2cf0d4e5f379c5705c6651306df5da93f39b64
Michal Cukierman <michal.cukierman@ds.pl>
2025-08-01 13:28:17 +0200

    Updated to pilot-based streamx.cloud

--- scripts/sx-cloud-deploy.sh
+++ scripts/sx-cloud-deploy.sh
@@ -5,7 +5,7 @@ PROJECT=${2:-ds-puresight}
 
 echo "Deploying to main cluster"
 export QUARKUS_PROFILE=sxcloud
-export KUBECONFIG="$STREAMX_CLOUD_INFRA_PATH/clusters/master/streamx-cloud-master_kubeconfig.yaml"
+export KUBECONFIG="$STREAMX_CLOUD_INFRA_PATH/clusters/pilot/streamx-cloud-pilot_kubeconfig.yaml"
 streamx deploy -n "$PROJECT" -f mesh/mesh.yaml
 echo "Deployment to main cluster complete."
 
