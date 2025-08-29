525e936d88bd21c5ff8e29e08c7e4751ce5fd3b2
Rafał Głowacz <rafal.glowacz@ds.pl>
2025-08-29 12:27:02 +0200

    SXPR-77 Switch terraform StreamX to mesh-light.yaml

--- terraform/scripts/deploy-streamx.sh
+++ terraform/scripts/deploy-streamx.sh
@@ -24,7 +24,7 @@ echo "%cloud.streamx.accelerator.ip=$streamx_ingress_ip" > "$SETUP_ENV_SCRIPT_DI
 "$SETUP_ENV_SCRIPT_DIR"/verify_secrets.sh
 
 pushd "${SETUP_ENV_SCRIPT_DIR}/../../" || exit
-export QUARKUS_PROFILE=cloud && streamx --accept-license deploy -f "$SETUP_ENV_SCRIPT_DIR/../../mesh/mesh.yaml"
+export QUARKUS_PROFILE=cloud && streamx --accept-license deploy -f "$SETUP_ENV_SCRIPT_DIR/../../mesh/mesh-light.yaml"
 popd || exit
 
 echo ""
